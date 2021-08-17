2a2835db2d8545dd632440edf0bbb55a4c44d8cd
muhammadamir98 <98.muhammadamir@gmail.com>
2021-08-17 17:02:32 +0430

    part project and art part1

---
+++ DeyCard_MVC/Models/Projects.cs
@@ -0,0 +1,10 @@
+﻿namespace DeyCard_MVC.Models
+{
+    public class Projects
+    {
+        public long Id { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        public string client { set; get; }
+    }
+}
