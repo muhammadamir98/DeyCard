20230d55464f277b9dee1ff2ef8e36813fe2d738
muhammadamir98 <98.muhammadamir@gmail.com>
2021-08-18 13:47:40 +0430

    View Components part1

--- DeyCard_MVC/Models/Projects.cs
+++ DeyCard_MVC/Models/Projects.cs
@@ -5,6 +5,16 @@
         public long Id { set; get; }
         public string Name { set; get; }
         public string Description { set; get; }
-        public string client { set; get; }
+        public string Image { get; set; }
+        public string Client { set; get; }
+
+        public Projects(long id, string name, string description, string image, string client)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Client = client;
+        }
     }
 }
---
+++ DeyCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DeyCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+
+namespace DeyCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Projects>()
+            {
+                new Projects(1, "mahsa", "chemistery", "project-1.jpg", "tandis"),
+                new Projects(2, "muhammad amir", "mathematics", "project-2.jpg", "tandis"),
+                new Projects(3, "hooman", "power", "project-3.jpg", "iran"),
+                new Projects(4, "mobina", "law", "project-4.jpg", "iran")
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
